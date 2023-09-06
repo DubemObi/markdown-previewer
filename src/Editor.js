26a3a121cdde574f4deab65759bd038889ddfc07
unknown <dubemobiwon@gmail.com>
2023-09-06 10:20:11 +0100

    preview changes

--- src/Editor.js
+++ src/Editor.js
@@ -12,7 +12,7 @@ class Editor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: "",
+      input: entry,
       previewerMaximize: false,
       editorMaximize: false,
     };
@@ -46,17 +46,7 @@ class Editor extends React.Component {
   }
   render() {
     // const display = marked.parse(this.state.input);
-    const display = marked.parse(this.state.input, {
-      gfm: true,
-      highlight: false,
-      tables: false,
-      breaks: true,
-      pedantic: false,
-      sanitize: true,
-      smartLists: true,
-      smartypants: false,
-      langPrefix: false,
-    });
+    // const display =
     if (this.state.editorMaximize) {
       return (
         <div>
@@ -99,8 +89,22 @@ class Editor extends React.Component {
           </div>
 
           <div id="preview">
-            <MarkedPreview data={display} />
-            {/* <div dangerouslySetInnerHTML={{ __html: display }} /> */}
+            <div
+              id="preview-content"
+              dangerouslySetInnerHTML={{
+                __html: marked.parse(this.state.input, {
+                  gfm: true,
+                  highlight: false,
+                  tables: false,
+                  breaks: true,
+                  pedantic: false,
+                  sanitize: true,
+                  smartLists: true,
+                  smartypants: false,
+                  langPrefix: false,
+                }),
+              }}
+            />
           </div>
         </div>
       );
@@ -125,6 +129,7 @@ class Editor extends React.Component {
             rows="14"
             onChange={this.handleChange}
             value={this.state.input}
+            spellCheck="false"
           ></textarea>
           <div id="preview-title">
             <div>
@@ -139,9 +144,23 @@ class Editor extends React.Component {
             />
           </div>
 
-          <div id="preview">
-            <MarkedPreview data={display} />
-            {/* <div dangerouslySetInnerHTML={{ __html: display }} /> */}
+          <div id="preview-content">
+            <div
+              id="preview"
+              dangerouslySetInnerHTML={{
+                __html: marked.parse(this.state.input, {
+                  gfm: true,
+                  highlight: false,
+                  tables: false,
+                  breaks: true,
+                  pedantic: false,
+                  sanitize: true,
+                  smartLists: true,
+                  smartypants: false,
+                  langPrefix: false,
+                }),
+              }}
+            />
           </div>
         </div>
       );
@@ -150,16 +169,46 @@ class Editor extends React.Component {
 }
 export default Editor;
 
-class MarkedPreview extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div
-        id="preview-content"
-        dangerouslySetInnerHTML={{ __html: this.props.data }}
-      />
-    );
+const entry = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
   }
 }
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)`;
